5fbef6718330509ddd775eb4d9c8d693c9837437
Alex Danoff <aadanoff@gmail.com>
2015-09-07 00:11:04 -0400

    Switch to using `ls` instead of `gls`
    `gls` formats output weirdly, so switch back to `ls` with color

--- system/aliases.bash
+++ system/aliases.bash
@@ -1,10 +1,10 @@
 # grc overides for ls
 #   Made possible through contributions from generous benefactors like
 #   `brew install coreutils`
-if $(gls &>/dev/null)
-then
-  alias ls="gls -F --color"
-  alias l="gls -lAh --color"
-  alias ll="gls -l --color"
-  alias la='gls -A --color'
-fi
+#if $(gls &>/dev/null)
+#then
+alias ls="ls -F -G"
+alias l="ls -lAh -G"
+alias ll="ls -l -G"
+alias la='ls -A -G'
+#fi
