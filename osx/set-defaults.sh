f6bebbd86b9fb442732c052b0cf85830589d69be
Alex Danoff <aadanoff@gmail.com>
2015-10-01 22:54:26 -0400

    Suppress iTerm2 quit message
    OS X set-defaults script now suppresses the iTerm2 quit prompt.

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -39,3 +39,6 @@ defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
 defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+
+# Stop iterm from displaying quit prompt
+defaults write com.googlecode.iterm2 PromptOnQuit -bool false
